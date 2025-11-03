4e3dfd1264f862a1377e8e36fcf4a0866a6b7bea
Krrishbalana <krrishbalana@gmail.com>
2025-11-04 02:45:00 +0530

    add About, Navbar, Hero sections and Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 function App() {
-  return <div className="bg-amber-200">Hello</div>;
+  return (
+    <div className="container mx-auto max-w-7xl">
+      {/* Navbar */}
+      <Navbar />
+      {/* hero */}
+      <Hero />
+      {/* about */}
+      <About />
+      {/* projects */}
+      {/* experience */}
+      {/* testimonials */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
 }
 
 export default App;
