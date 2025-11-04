fecd12860facaa5da2136953b19fc5499a01d410
Krrishbalana <krrishbalana@gmail.com>
2025-11-04 13:33:35 +0530

    add Footer section and update social links in constants

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
       {/* testimonials */}
       {/* contact */}
       <Contact />
-      {/* footer */}
+      <Footer />
     </div>
   );
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -156,17 +156,17 @@ export const myProjects = [
 export const mySocials = [
   {
     name: "WhatsApp",
-    href: "",
+    href: "https://wa.me/8690909867",
     icon: "/assets/socials/whatsApp.svg",
   },
   {
-    name: "Linkedin",
-    href: "https://www.linkedin.com/in/ali-sanati/",
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/krrish-balana",
     icon: "/assets/socials/linkedIn.svg",
   },
   {
     name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    href: "https://www.instagram.com/krrishbalana/",
     icon: "/assets/socials/instagram.svg",
   },
 ];
